c61a8bb35bceadbf840b2208604736a108db778e
Upendra Kumar <156185765+Upendra090@users.noreply.github.com>
2024-05-13 18:27:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import CardList from "./components/CardList";
 import Header from "./components/Header";
 
 function App() {
+   useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("../sw.js").then(() => {
+          console.log("Service Worker Registered");
+        });
+      });
+    }
+  });
+  
   return (
     <>
       <Header />
