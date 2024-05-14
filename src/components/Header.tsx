95a4c1cba4167522fcc9d14bc4be583a79d6259a
Upendra Kumar <156185765+Upendra090@users.noreply.github.com>
2024-05-14 18:02:05 +0530

    Delete src/components directory

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="w-full text-center py-2 border-b border-gray-800">
-      <h3 className="text-purple-800 text-2xl font-semibold">
-        Marvel Characters
-      </h3>
-    </header>
-  );
-}
